55d956261d1d9ae8688a175bba330e702843b5e7
Chr1sDev <christopheranderson3@me.com>
2021-03-12 17:25:43 -0600

    add package

--- repo.js
+++ repo.js
@@ -2,7 +2,8 @@ exports.name = "Chr1s' Repo"
 exports.description = "Jailbreak go brrr"
 
 exports.packages = [
-	'https://github.com/Chr1sDev/hidedots/releases/download/v1.0.1/com.chr1s.hidedots_1.0.1_iphoneos-arm.deb'
+	'https://github.com/Chr1sDev/hidedots/releases/download/v1.0.1/com.chr1s.hidedots_1.0.1_iphoneos-arm.deb',
+	'https://github.com/Chr1sDev/ReachTheNekos/releases/download/v1.0.3/com.chr1s.reachthenekos_1.0.3_iphoneos-arm.deb'
 ]
 
 exports.icons = {
