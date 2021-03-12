5bc9dc85086e365eb159b8394617a6747de62629
Chr1sDev <christopheranderson3@me.com>
2021-03-12 15:56:54 -0600

    pog

--- repo.js
+++ repo.js
@@ -1,15 +1,10 @@
-exports.name = 'Repo on Vercel'
-exports.description = "Repo on Vercel's example repository"
+exports.name = "Chr1s' Repo"
+exports.description = "Jailbreak go brrr"
 
 exports.packages = [
-	'https://github.com/dimitarnestorov/RepoOnVercel/releases/download/red-1.0.1/example.red_1.0.1_iphoneos-arm.deb',
-	'https://github.com/dimitarnestorov/RepoOnVercel/releases/download/red-1.0.0/example.red_1.0.0_iphoneos-arm.deb',
-	'https://github.com/dimitarnestorov/RepoOnVercel/releases/download/green-1.0.0/example.green_1.0.0_iphoneos-arm.deb',
-	'https://github.com/dimitarnestorov/RepoOnVercel/releases/download/blue-1.0.0/example.blue_1.0.0_iphoneos-arm.deb',
+	'https://github.com/Chr1sDev/hidedots/releases/download/v1.0.1/com.chr1s.hidedots_1.0.1_iphoneos-arm.deb'
 ]
 
 exports.icons = {
-	Red: 'assets/red/icon.png',
-	Blue: 'assets/blue/icon.png',
-	Green: 'assets/green/icon.png',
+	Red: 'assets/red/icon.png'
 }
