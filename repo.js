dcb9f9c3320768eefe4895d29bd9de51805dc9ea
Chr1sDev <chris@chr1s.dev>
2021-04-04 22:45:20 -0500

    update digitalclock 1.0.6

--- repo.js
+++ repo.js
@@ -5,7 +5,7 @@ exports.packages = [
 	'https://github.com/Chr1sDev/hidedots/releases/download/v1.0.1/com.chr1s.hidedots_1.0.1_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/ReachTheNekos/releases/download/v1.0.3/com.chr1s.reachthenekos_1.0.3_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/discordvideoloop/releases/download/v1.0.0/com.chr1s.discordvideoloop_1.0.0_iphoneos-arm.deb',
-	'https://github.com/Chr1sDev/digitalclock/releases/download/v1.0.5/com.chr1s.digitalclock_1.0.5_iphoneos-arm.deb'
+	'https://github.com/Chr1sDev/digitalclock/releases/download/v1.0.6/com.chr1s.digitalclock_1.0.6_iphoneos-arm.deb'
 ]
 
 exports.icons = {
