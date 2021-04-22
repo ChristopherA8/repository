233c0562044c835bf600877e7ddb74a0e37b6411
Chr1sDev <chris@chr1s.dev>
2021-04-21 20:09:15 -0500

    changed: HideDots.js, Packages.js

--- pages/api/Packages.js
+++ pages/api/Packages.js
@@ -6,9 +6,9 @@ const spaceRegExp = /\s/g
 export default (req, res) => {
 	const url = getRepoUrl(req)
 	const result = []
-	let sileoDepictions = {
-		discord_video_loop: `${url}SileoDepictions/discord-video-loop.json`,
-	}
+	// let sileoDepictions = {
+	// 	discord_video_loop: `${url}SileoDepictions/discord-video-loop.json`,
+	// }
 	for (const name in packages) {
 		const versions = packages[name]
 		for (const version in versions) {
@@ -21,9 +21,9 @@ export default (req, res) => {
 			icons[name] && strings.push(`Icon: ${url}${icons[name]}`)
 			strings.push(`Depiction: ${url}${name.replace(spaceRegExp, '-')}`)
 
-			if (sileoDepictions[name.replace(spaceRegExp, '_')]) {
-				strings.push(`SileoDepiction: ${sileoDepictions[name.replace(spaceRegExp, '_')]}`)
-			}
+			// if (sileoDepictions[name.replace(spaceRegExp, '_')]) {
+			// 	strings.push(`SileoDepiction: ${sileoDepictions[name.replace(spaceRegExp, '_')]}`)
+			// }
 
 			result.push(strings.join('\n'))
 		}
