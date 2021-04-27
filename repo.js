bcd185a6fbf2161607c74259cb464d7405023459
Chr1sDev <christopheraanderson2@gmail.com>
2021-04-26 22:22:25 -0500

    add NoNewContactPhotoAvailableBanner

--- repo.js
+++ repo.js
@@ -6,10 +6,12 @@ exports.packages = [
 	'https://github.com/Chr1sDev/ReachTheNekos/releases/download/v1.0.3/com.chr1s.reachthenekos_1.0.3_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/discordvideoloop/releases/download/v1.0.0/com.chr1s.discordvideoloop_1.0.0_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/chatwall/releases/download/v1.0.0-beta/com.chr1s.chatwall_1.0.0_iphoneos-arm.deb',
+	'https://github.com/Chr1sDev/nonewcontactphotoavailablebanner/releases/download/v1.0.0/com.chr1s.nonewcontactphotoavailablebanner_1.0.0_iphoneos-arm.deb',
 ]
 
 exports.icons = {
 	ChatWall: 'assets/chatWall/icon.png',
 	HideDots: 'assets/hideDots/icon.png',
 	ChatWall_Beta: 'assets/chatWall/icon.png',
+	NoNewContactPhotoAvailableBanner: 'assets/NoNewContactPhotoAvailableBanner/icon.png'
 }
