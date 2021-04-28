f580548aeb32710deae82423d7e9e6c98822377c
Chr1sDev <chris@chr1s.dev>
2021-04-28 15:25:12 -0500

    add depiction for gameCenterBanner tweak

---
+++ pages/HideGameCenterBanner.js
@@ -0,0 +1,13 @@
+import Depiction from '../components/Depiction'
+
+export default function HideGameCenterBanner() {
+	return (
+		<Depiction
+			name="HideGameCenterBanner"
+			github="https://github.com/Chr1sDev/hidegamecenterbanner"
+			subtitle="Self Explanatory"
+		>
+			Hide the "Signed in as " banner when opening Game Center compatible games
+		</Depiction>
+	)
+}
