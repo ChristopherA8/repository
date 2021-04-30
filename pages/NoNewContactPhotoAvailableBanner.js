0d2070aaf9bd1d25885e9bfd94176b77da936d17
Chr1sDev <chris@chr1s.dev>
2021-04-29 22:25:43 -0500

    pog

---
+++ pages/NoNewContactPhotoAvailableBanner.js
@@ -0,0 +1,13 @@
+import Depiction from '../components/Depiction'
+
+export default function NoNewContactPhotoAvailableBanner() {
+	return (
+		<Depiction
+			name="NoNewContactPhotoAvailableBanner"
+			github="https://github.com/Chr1sDev/NoNewContactPhotoAvailableBanner"
+			subtitle="Self Explanatory"
+		>
+			Hide the 'New contact photo available' banner in iMessage chats
+		</Depiction>
+	)
+}
