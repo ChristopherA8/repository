1b21263e26c87596fab1b39a807421879d6df037
Chr1sDev <chris@chr1s.dev>
2021-04-29 21:51:38 -0500

    add newtab to repo

---
+++ pages/NewTab.js
@@ -0,0 +1,9 @@
+import Depiction from '../components/Depiction'
+
+export default function NewTab() {
+	return (
+		<Depiction name="NewTab" github="https://github.com/Chr1sDev/NewTab" subtitle="New Tab Shortcut">
+			Add new tab button to the safari toolbar
+		</Depiction>
+	)
+}
