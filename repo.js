b2fdd6bce8cf37f757686763aaa863b8ce403375
Chr1sDev <chris@chr1s.dev>
2021-04-30 20:35:57 -0500

    update chatwall beta

--- repo.js
+++ repo.js
@@ -6,6 +6,7 @@ exports.packages = [
 	'https://github.com/Chr1sDev/ReachTheNekos/releases/download/v1.0.3/com.chr1s.reachthenekos_1.0.3_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/discordvideoloop/releases/download/v1.0.0/com.chr1s.discordvideoloop_1.0.0_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/chatwall/releases/download/v1.0.0-beta/com.chr1s.chatwall_1.0.0_iphoneos-arm.deb',
+	'https://github.com/Chr1sDev/chatwall/releases/download/v1.0.1-beta/com.chr1s.chatwall_1.0.1_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/nonewcontactphotoavailablebanner/releases/download/v1.0.0/com.chr1s.nonewcontactphotoavailablebanner_1.0.0_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/hidegamecenterbanner/releases/download/v1.0.0/com.chr1s.hidegamecenterbanner_1.0.0_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/newtab/releases/download/v1.0.0/com.chr1s.newtab_1.0.0_iphoneos-arm.deb',
