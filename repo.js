6249e3b7f8def579e4c2d72c63d8a18d9b4c7108
Chr1sDev <chris@chr1s.dev>
2021-05-07 12:32:16 -0500

    update current

--- repo.js
+++ repo.js
@@ -5,6 +5,7 @@ exports.packages = [
 	'https://github.com/Chr1sDev/hidedots/releases/download/v1.0.1/com.chr1s.hidedots_1.0.1_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/ReachTheNekos/releases/download/v1.0.3/com.chr1s.reachthenekos_1.0.3_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/discordvideoloop/releases/download/v1.0.0/com.chr1s.discordvideoloop_1.0.0_iphoneos-arm.deb',
+	// ChatWall (now on packix)
 	// 'https://github.com/Chr1sDev/chatwall/releases/download/v1.0.0-beta/com.chr1s.chatwall_1.0.0_iphoneos-arm.deb',
 	// 'https://github.com/Chr1sDev/chatwall/releases/download/v1.0.1-beta/com.chr1s.chatwall_1.0.1_iphoneos-arm.deb',
 	// 'https://github.com/Chr1sDev/chatwall/releases/download/v1.0.2/com.chr1s.chatwall_1.0.2_iphoneos-arm.deb',
@@ -13,7 +14,9 @@ exports.packages = [
 	'https://github.com/Chr1sDev/hidegamecenterbanner/releases/download/v1.0.0/com.chr1s.hidegamecenterbanner_1.0.0_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/newtab/releases/download/v1.0.0/com.chr1s.newtab_1.0.0_iphoneos-arm.deb',
 	'https://github.com/Chr1sDev/tapflash/releases/download/v1.0.2/com.chr1s.tapflash_1.0.2_iphoneos-arm.deb',
+	// Current
 	'https://github.com/Chr1sDev/Current/releases/download/v1.0.0-beta1/com.chr1s.current_1.0.0_iphoneos-arm.deb',
+	'https://github.com/Chr1sDev/Current/releases/download/v1.0.0-beta2/com.chr1s.current_1.0.0-beta2_iphoneos-arm.deb',
 ]
 
 exports.icons = {
