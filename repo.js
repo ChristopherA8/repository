2075fb81a67641f4a0d6c5b1adf8f91002735577
Chr1sDev <chris@chr1s.dev>
2021-06-21 10:39:36 -0500

    pog

--- repo.js
+++ repo.js
@@ -1,5 +1,5 @@
 exports.name = "Chr1s' Repo"
-exports.description = 'Jailbreak go brrr'
+exports.description = 'This is a repo :)'
 
 exports.packages = [
 	'https://github.com/Chr1sDev/hidedots/releases/download/v1.0.1/com.chr1s.hidedots_1.0.1_iphoneos-arm.deb',
